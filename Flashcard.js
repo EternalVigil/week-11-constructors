bdec40fe9d5763ee4f394bc21d4ed54336fdf35b
Brandon <theeternalvigil@gmail.com>
2016-12-08 18:13:41 -0800

    initial Flashcard.js commit

---
+++ Flashcard.js
@@ -0,0 +1,10 @@
+/* Flashcard Constructor */
+
+function Flashcard (frontData, backData){
+	"use strict";
+	this.frontside = frontData;
+	this.backside = backData;
+	
+}
+
+module.exports = Flashcard;
